b6af8bd93139a47d1a1e93778f3e4c2895327fe7
Ian Mworia <53788784+mworiam@users.noreply.github.com>
2023-04-03 10:35:44 +0300

    Update sql_connection.py

--- Kibanda_Management_System_Backend/sql_connection.py
+++ Kibanda_Management_System_Backend/sql_connection.py
@@ -6,6 +6,6 @@ __cnx = None
 def get_sql_connection():
     global __cnx
     if __cnx is None:
-        __cnx = mysql.connector.connect( user ='root', password='12345', host = '127.0.0.1', database = 'grocery_store')
+        __cnx = mysql.connector.connect( user ='root', password='12345', host = '127.0.0.1', database = 'kibanda_store')
 
         return __cnx
